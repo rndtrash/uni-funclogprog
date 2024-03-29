177cb3fb9bd71fcda4dec3fda6084eb6f75ef636
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2024-03-30 01:54:40 +0300

    Restructure the lab1 folder

--- lab1/ancient_rus_family.pl
+++ lab1/ancient_rus_family.pl
@@ -64,3 +64,29 @@ husband(X, Y):- woman(X), man(Y), parent(X, Z), parent(Y, Z).
 
 /* husband(X) - найти мужа жены X */
 husband(X):- husband(X, Y), print(Y), nl, fail.
+
+/* grand_so(X, Y) - X является внуком Y */
+grand_so(X, Y):- man(X), parent(Z, X), parent(Y, Z).
+
+/* grand_sons(X) - внуки X. Пример: grand_sons(goluba). */
+grand_sons(X):- grand_so(Y, X), print(Y), nl, fail.
+
+/* grand_da(X, Y) - X является внучкой Y */
+grand_da(X, Y):- woman(X), parent(Z, X), parent(Y, Z).
+
+/*
+	grand_ma_and_da(X, Y) - X является бабушкой, а Y - её внучкой, либо наоборот
+
+	Сначала делается предположение, что X - внучка, а Y - прародитель (grandparent), тогда Y должна быть женщиной.
+	В противном случае делается предположение, что Y - внучка, а X - прародитель, тогда Y должна быть женщиной.
+*/
+grand_ma_and_da(X, Y):- grand_da(X, Y), woman(Y); grand_da(Y, X), woman(X).
+
+/* sibling(X, Y) - X является сиблингом (родным братом или сестрой) Y. Проверяется по общему отцу. */
+sibling(X, Y):- X \= Y, parent(Z, X), parent(Z, Y), man(Z).
+
+/* niece(X, Y) - X является племянницей (дочью брата/сестры) Y */
+niece(X, Y):- woman(X), parent(Z, X), sibling(Z, Y). /* , \+ parent(X, Y) */
+
+/* nieces(X) - все племянницы X */
+nieces(X):- niece(Y, X), print(Y), nl, fail.
